7122e96aa26807135eeb2726190264729fe2a9a7
SeungYn <kwls0407@gmail.com>
2024-12-05 00:29:54 +0900

    feat: msw 적용중

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://example.com/user', () => {
+  http.get('/user', () => {
     console.log('msw request');
     return HttpResponse.json({
       id: '1234',
